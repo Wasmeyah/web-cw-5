aa19977210cb3efdfbc257a21cda9ee240fdcae0
Wasmeyah <wasmya2013@gmail.com>
2021-01-02 20:25:52 +0300

    all done but not the third q

---
+++ script.js
@@ -0,0 +1,10 @@
+let MyAge = 22;
+
+console.log(MyAge);
+console.log(MyAge*5);
+console.log(`حين أبلغ 5 أضعاف عمري سيبدأ زحل باستقبال المبرمجين على سطحه`);
+
+let numbersofYears = 2021-1969 ;
+console.log(`${numbersofYears} هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر `)
+
+//إجابة السؤال الاول 
